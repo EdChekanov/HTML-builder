6d6f296f01c005cb895acb279920cdd367d8e455
EdChekanov <kide.da@gmail.com>
2021-11-07 16:22:04 +0300

    feat: add funct to 6 task

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,10 +1,12 @@
 const fs = require('fs');
 const path = require('path');
 
-const srcProjectDist = path.join(__dirname, 'test-files', 'project-dist2');
-const srcHTML = path.join(__dirname, 'test-files', 'project-dist2', 'index.html');
+// COMPONENTS
+
+const srcProjectDist = path.join(__dirname, 'project-dist');
+const srcHTML = path.join(__dirname, 'project-dist', 'index.html');
 const srcTemplate = path.join(__dirname, 'template.html');
-const srcComponents = path.join(__dirname, 'test-files', 'components');
+const srcComponents = path.join(__dirname, 'components');
 
 fs.promises.mkdir(srcProjectDist, {recursive: true});
 fs.readFile(srcTemplate, 'utf8', (err, data) => {
@@ -38,4 +40,92 @@ fs.readFile(srcTemplate, 'utf8', (err, data) => {
   }
 });
 
+// STYLES
 
+const bundleSrc = path.join(__dirname, 'project-dist', 'style.css');
+const stylesSrc = path.join(__dirname, 'styles');
+
+fs.writeFile(bundleSrc, '', (err) => {
+  if (err) console.log(err);
+})
+
+fs.readdir(stylesSrc, {withFileTypes: 'true'}, (err, files) => {
+  if (err) {
+    console.log(err);
+  } else {
+    files.forEach((file) => {
+      let src = path.join(stylesSrc, file.name);
+      if (file.isFile() && path.extname(src) === '.css') {
+        fs.readFile(src, (err, data) => {
+          if (err) {
+            console.log(err);
+          } else {
+            fs.appendFile(bundleSrc, data.toString(), (err) => {
+              if (err) console.log(err);
+            })
+          }
+        })
+      };
+    })
+  }
+})
+
+// ASSETS
+
+const srcDir = path.join(__dirname, 'assets');
+const srcCopyDir = path.join(__dirname, 'project-dist', 'assets');
+
+fs.promises.mkdir(srcCopyDir, {recursive: true});
+
+function copy(srcCopyFrom) {
+  fs.readdir(srcCopyFrom, {withFileTypes: 'true'}, (err, files) => {
+    if (err) {
+      console.log(err);
+    } else {
+        files.forEach((file) => {
+          if (file.isFile()) {
+            let src = path.join(srcCopyFrom, file);
+            let way = path.join(srcCopyDir, file);
+            fs.copyFile(src, way, (err) => {
+                if (err) console.log(err);
+            })
+          } else {
+            let srcToDir = path.join(srcCopyDir, file.name);
+            fs.promises.mkdir(srcToDir, {recursive: true});
+            copy(srcToDir);
+          }
+          });
+    }
+}
+);
+};
+
+function remove(srcCopyTo) {
+  fs.readdir(srcCopyTo, (err, files) => {
+    if (err) {
+      console.log(err);
+    } else {
+        files.forEach((file) => {
+          if (file.isFile()) {
+            let src = path.join(srcDir, file);
+            let srcCopy = path.join(srcCopyTo, file);
+            fs.access(src, (err) => {
+                if (err) {
+                    fs.rm(srcCopy, {recursive: true }, (err) => {
+                        if (err) console.log(err);
+                    });
+                }
+            })
+          } else {
+            let srcToDir = path.join(srcCopyTo, file.name);
+            remove(srcToDir);
+          }
+        });
+    }
+})
+};
+
+setTimeout(() => {
+  copy(srcDir);
+  remove(srcCopyDir);
+}, 100);
